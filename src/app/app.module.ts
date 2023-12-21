582bbe8f7cbed27666d9c932070114e7ac9718a8
aya-hadebe1996 <boshiwehadebe@gmail.com>
2023-12-21 23:29:04 +0200

    Assignment_1

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component'; 
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
